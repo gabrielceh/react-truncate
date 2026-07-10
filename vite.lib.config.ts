d9aac31eb22b732acf89c28479ec15d85fac3b42
gabrielceh <gabrielcehu@gmail.com>
2026-07-10 11:11:59 -0500

    preparando para publicar

---
+++ vite.lib.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({ tsconfigPath: resolve(__dirname, "tsconfig.lib.json") }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "ReactTruncate",
+      formats: ["es", "cjs"],
+      fileName: (format) => (format === "es" ? "index.js" : "index.cjs"),
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "jsxRuntime",
+        },
+      },
+    },
+    cssCodeSplit: false,
+    outDir: "lib",
+  },
+});
